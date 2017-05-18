7fc7da1a257f2684de5dfd4f6d13a9be5d3cdb42
Henning Weiss <hwe@it-minds.dk>
2017-05-18 11:37:19 +0200

    Replaced old azure deploy.

--- content/itm-mvc/pipeline/deploy-to-azure.sh
+++ content/itm-mvc/pipeline/deploy-to-azure.sh
@@ -3,39 +3,28 @@
 # These variables should be defined in the Builds environment variables.
 # Retrieve them from an Azure Web App's Publishing Profile
 # (.publishsettings).
-# - FTP_HOST (e.g.: waws-prod-bay-039.ftp.azurewebsites.windows.net)
-# - FTP_USERNAME (e.g.: webappname\$webappname)
-# - FTP_PASSWORD
-# - FTP_SITE_ROOT (e.g.: /site/wwwroot)
-
-# Upload all files recursively from local directory to the remote
-# directory under the Azure Web App:
-# NOTE: If the FTP_PASSWORD environment variable was added with the
-# "Secured" checkbox checked,
-#       then the password will not be shown in cleartext in the build
-#       logs, and will instead be replaced
-#       with the "$FTP_PASSWORD" token. Another cool feature of
-#       Bitbucket Pipelines.
-
-deploy_path=$1
+# - AZURE_DEPLOY_URL (e.g.: https://webappname.scm.azurewebsites.net/)
+# - AZURE_DEPLOY_USERNAME (e.g.: username)
+# - AZURE_DEPLOY_PASSWORD
 
 apt-get update --quiet
-apt-get --yes install ncftp --quiet
+apt-get --yes install zip curl --quiet
 
-cd $deploy_path
+PUBLISH_PATH=$1
+
+PUBLISH_ZIP="publish.zip"
 
 # Hack to clean unnecessary runtimes
-rm runtimes/linux-x64 -Rf
-rm runtimes/osx-x64 -Rf
+rm $PUBLISH_PATH/runtimes/linux-x64 -Rf
+rm $PUBLISH_PATH/runtimes/osx-x64 -Rf
 
-# 0) Hack to reset IIS
-ncftpput -v -u "$FTP_USERNAME" -p "$FTP_PASSWORD" -R $FTP_HOST $FTP_SITE_ROOT "web.config"
+cd $PUBLISH_PATH
+zip -r ../$PUBLISH_ZIP .
+cd ..
 
-# 1) Upload all files:
-ncftpput -v -u "$FTP_USERNAME" -p "$FTP_PASSWORD" -R $FTP_HOST $FTP_SITE_ROOT *
+curl -H "Content-Type: application/json" -X POST -d '{"command":"rm site/wwwroot/web.config", "path":""}' "$AZURE_DEPLOY_URL/api/command" --user $AZURE_DEPLOY_USERNAME:$AZURE_DEPLOY_PASSWORD
 
-cd -
+curl -X PUT --data-binary @$PUBLISH_ZIP "$AZURE_DEPLOY_URL/api/zip/site/wwwroot/" --user $AZURE_DEPLOY_USERNAME:$AZURE_DEPLOY_PASSWORD
 
-echo Finished uploading files to $FTP_HOST$FTP_SITE_ROOT.
-
-bash ./pipeline/hipchat.sh -v v2 -i "Finished uploading files to $FTP_HOST$FTP_SITE_ROOT"
+HIPCHAT_URL=`echo $AZURE_DEPLOY_URL | cut -c9-`
+echo "Deployed to $HIPCHAT_URL"

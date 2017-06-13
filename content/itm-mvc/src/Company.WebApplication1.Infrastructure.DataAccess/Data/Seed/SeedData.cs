9248f8fb34df8957ad3616cc48251f2122962943
esd <esd@it-minds.dk>
2017-06-13 23:47:34 +0200

    Changed foreach to AddRange

--- content/itm-mvc/src/Company.WebApplication1.Infrastructure.DataAccess/Data/Seed/SeedData.cs
+++ content/itm-mvc/src/Company.WebApplication1.Infrastructure.DataAccess/Data/Seed/SeedData.cs
@@ -34,20 +34,17 @@ namespace Company.WebApplication1.Infrastructure.DataAccess.Data.Seed
             if (!context.Students.Any())
             {
                 var students = new Student[]
-                            {
-                                new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
-                                new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
-                                new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
-                                new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
-                                new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
-                                new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
-                                new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
-                                new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
-                            };
-                foreach (Student s in students)
                 {
-                    context.Students.Add(s);
-                }
+                    new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                    new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                    new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                    new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                    new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                    new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+                    new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                    new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+                };
+                context.Students.AddRange(students);
                 context.SaveChanges();
 
                 var courses = new Course[]
@@ -60,10 +57,7 @@ namespace Company.WebApplication1.Infrastructure.DataAccess.Data.Seed
                     new Course{Id=2021,Title="Composition",Credits=3},
                     new Course{Id=2042,Title="Literature",Credits=4}
                 };
-                foreach (Course c in courses)
-                {
-                    context.Courses.Add(c);
-                }
+                context.Courses.AddRange(courses);
                 context.SaveChanges();
 
                 var enrollments = new Enrollment[]
@@ -81,10 +75,7 @@ namespace Company.WebApplication1.Infrastructure.DataAccess.Data.Seed
                     new Enrollment{StudentId=6,CourseId=1045},
                     new Enrollment{StudentId=7,CourseId=3141,Grade=Grade.A},
                 };
-                foreach (Enrollment e in enrollments)
-                {
-                    context.Enrollments.Add(e);
-                }
+                context.Enrollments.AddRange(enrollments);
                 context.SaveChanges();
             }
 #endif

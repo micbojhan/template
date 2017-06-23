cacd8d3dcacb29809e6a92abe2e99771f24da4a7
Henning Weiss <hwe@it-minds.dk>
2017-06-23 14:15:42 +0200

    Minor improvements to bitbucket-pipelines and azure deloy.

--- content/itm-mvc/pipeline/deploy-to-azure.sh
+++ content/itm-mvc/pipeline/deploy-to-azure.sh
@@ -3,14 +3,17 @@
 # These variables should be defined in the Builds environment variables.
 # Retrieve them from an Azure Web App's Publishing Profile
 # (.publishsettings).
-# - AZURE_DEPLOY_URL (e.g.: https://webappname.scm.azurewebsites.net/)
 # - AZURE_DEPLOY_USERNAME (e.g.: username)
 # - AZURE_DEPLOY_PASSWORD
 
 apt-get update --quiet
 apt-get --yes install zip curl --quiet
 
-PUBLISH_PATH=$1
+
+WEBAPP_NAME=$1
+PUBLISH_PATH=$2
+
+AZURE_DEPLOY_URL="https://$WEBAPP_NAME.scm.azurewebsites.net"
 
 PUBLISH_ZIP="publish.zip"
 
@@ -18,13 +21,18 @@ PUBLISH_ZIP="publish.zip"
 rm $PUBLISH_PATH/runtimes/linux-x64 -Rf
 rm $PUBLISH_PATH/runtimes/osx-x64 -Rf
 
+
+# Pack deploy
 cd $PUBLISH_PATH
 zip -r ../$PUBLISH_ZIP .
 cd ..
 
-curl -H "Content-Type: application/json" -X POST -d '{"command":"rm site/wwwroot/web.config", "path":""}' "$AZURE_DEPLOY_URL/api/command" --user $AZURE_DEPLOY_USERNAME:$AZURE_DEPLOY_PASSWORD
+# Reset webserver by deleting web.config
+curl --fail -H "Content-Type: application/json" -X POST -d '{"command":"rm site/wwwroot/web.config", "path":""}' "$AZURE_DEPLOY_URL/api/command" --user $AZURE_DEPLOY_USERNAME:$AZURE_DEPLOY_PASSWORD
 
-curl -X PUT --data-binary @$PUBLISH_ZIP "$AZURE_DEPLOY_URL/api/zip/site/wwwroot/" --user $AZURE_DEPLOY_USERNAME:$AZURE_DEPLOY_PASSWORD
+# Upload packed app
+curl --fail -X PUT --data-binary @$PUBLISH_ZIP "$AZURE_DEPLOY_URL/api/zip/site/wwwroot/" --user $AZURE_DEPLOY_USERNAME:$AZURE_DEPLOY_PASSWORD
+
 
 HIPCHAT_URL=`echo $AZURE_DEPLOY_URL | cut -c9-`
 echo "Deployed to $HIPCHAT_URL"
